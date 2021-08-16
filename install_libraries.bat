4f23cae21f4aa55ae9c5b743e0174fa5eecba0ea
ju1ce <likozar.janus@gmail.com>
2021-08-16 14:11:47 +0200

    Removed pytorch from required libraries

--- install_libraries.bat
+++ install_libraries.bat
@@ -5,7 +5,6 @@ python\python.exe -m pip install --upgrade pip
 python\python.exe -m pip install numpy
 python\python.exe -m pip install tensorflow
 python\python.exe -m pip install tensorflow-hub
-python\python.exe -m pip install torch
 python\python.exe -m pip install opencv-python
 python\python.exe -m pip install scipy
 
